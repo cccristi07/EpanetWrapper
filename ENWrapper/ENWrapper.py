e8942dc3e2eeebd374b188c2c109bf2db982db3c
Cazan Cristian <cristian.cazan@sparktech.ro>
2018-04-20 13:23:52 +0300

    Refactoring

--- ENWrapper/ENWrapper.py
+++ ENWrapper/ENWrapper.py
@@ -1,4 +1,4 @@
-#epanet toolkit
+# epanet toolkit
 import json
 
 import numpy as np
@@ -14,16 +14,14 @@ from plotly.offline import plot
 # extending the EPANetSimulation class to ease acces to
 # simulation routines
 class ENSim(EPANetSimulation):
-
     EN_INIT = 10
 
-    def __init__(self, inputFileName, pdd=False):
+    def __init__(self, network_file, pdd=False):
         self.json_sim = {}
-        super().__init__(inputFileName, pdd)
+        super().__init__(network_file, pdd)
 
     def set_emitter(self, node_index, emitter_val):
         self.ENsetnodevalue(node_index, EN_EMITTER, emitter_val)
-
 
     def set_emitters(self, emitter_info=None):
 
@@ -36,12 +34,11 @@ class ENSim(EPANetSimulation):
             for node_index, emitter_val in emitter_info:
                 self.set_emitter(node_index, emitter_val)
 
-    def get_nodes_data(self, data_query, emitter=(1,0)):
+    def get_nodes_data(self, data_query, emitter=(1, 0)):
 
-        no_nodes = ENSim._getNcheck(self.ENgetcount(EN_NODECOUNT)) - ENSim._getNcheck(self.ENgetcount(EN_TANKCOUNT))
+        no_nodes = ENSim._getncheck(self.ENgetcount(EN_NODECOUNT)) - ENSim._getncheck(self.ENgetcount(EN_TANKCOUNT))
         t_step = 1
         node_values = {}
-
 
         for queries in data_query:
             node_values[queries] = [[] for _ in range(no_nodes)]
@@ -51,7 +48,7 @@ class ENSim(EPANetSimulation):
 
         # initialize network for hydraulic process
 
-        ENSim._getNcheck(self.ENinitH(ENSim.EN_INIT))
+        ENSim._getncheck(self.ENinitH(ENSim.EN_INIT))
 
         while t_step > 0:
 
@@ -59,10 +56,10 @@ class ENSim(EPANetSimulation):
 
             for node_index in range(1, no_nodes + 1):
                 for query_type in data_query:
-                    ret_val = ENSim._getNcheck(self.ENgetnodevalue(node_index, eval(query_type)))
-                    node_values[query_type][node_index-1].append(ret_val)
+                    ret_val = ENSim._getncheck(self.ENgetnodevalue(node_index, eval(query_type)))
+                    node_values[query_type][node_index - 1].append(ret_val)
 
-            t_step = ENSim._getNcheck(self.ENnextH())
+            t_step = ENSim._getncheck(self.ENnextH())
 
         for key in node_values:
             node_values[key] = np.transpose(node_values[key]).tolist()
@@ -82,14 +79,14 @@ class ENSim(EPANetSimulation):
         link_values["EMITTER_VAL"] = emitter[1]
 
         while t_step > 0:
-            ENSim._getNcheck(self.ENrunH())
+            ENSim._getncheck(self.ENrunH())
 
             for link_index in range(1, no_links + 1):
                 for query_type in data_query:
-                    ret_val = ENSim._getNcheck(self.ENgetlinkvalue(link_index, eval(query_type)))
-                    link_values[query_type][link_index-1].append(ret_val)
+                    ret_val = ENSim._getncheck(self.ENgetlinkvalue(link_index, eval(query_type)))
+                    link_values[query_type][link_index - 1].append(ret_val)
 
-            t_step = ENSim._getNcheck(self.ENnextH())
+            t_step = ENSim._getncheck(self.ENnextH())
 
         for key in link_values:
             link_values[key] = np.transpose(link_values[key]).tolist()
@@ -97,7 +94,7 @@ class ENSim(EPANetSimulation):
         return link_values
 
     def query_network(self, sim_dict):
-        '''
+        """
         :param sim_dict: a dict containing info about the network
         has the form
         {
@@ -125,21 +122,20 @@ class ENSim(EPANetSimulation):
 
         }
 
-        '''
+        """
 
         # for the moment i'll treat only hydraulic simulations :)
 
         # initialize network simulaton
 
-        ENSim._getNcheck(self.ENopenH())
+        ENSim._getncheck(self.ENopenH())
 
         # initialize session
-        ENSim._getNcheck(self.ENinitH(ENSim.EN_INIT))
+        ENSim._getncheck(self.ENinitH(ENSim.EN_INIT))
 
         node_query = False
         link_query = False
         simulations = False
-
 
         # check json for querried data
 
@@ -147,36 +143,38 @@ class ENSim(EPANetSimulation):
         try:
             if sim_dict["query"]["nodes"]:
                 node_query = True
-        except:
+        except KeyError:
             node_query = False
 
         # link info
         try:
             if sim_dict["query"]["links"]:
                 link_query = True
-        except:
+        except KeyError:
             link_query = False
 
         # emitter info:
         try:
             simulations = sim_dict["emitter_values"]
-        except:
+        except KeyError:
             simulations = False
 
         if simulations:
             node_values = []
             link_values = []
 
-            for node_index, emitter_value in  simulations:
+            for node_index, emitter_value in simulations:
                 print("Simulating emitter in node no{}".format(node_index))
 
                 self.set_emitter(node_index, emitter_value)
 
                 if node_query:
-                    node_values.append(self.get_nodes_data(sim_dict["query"]["nodes"], emitter=(node_index, emitter_value)))
+                    node_values.append(
+                        self.get_nodes_data(sim_dict["query"]["nodes"], emitter=(node_index, emitter_value)))
 
                 if link_query:
-                    link_values.append(self.get_links_data(sim_dict["query"]["links"], emitter=(node_index, emitter_value)))
+                    link_values.append(
+                        self.get_links_data(sim_dict["query"]["links"], emitter=(node_index, emitter_value)))
 
                 # reset emitter values everywhere in network
                 self.set_emitters()
@@ -198,30 +196,27 @@ class ENSim(EPANetSimulation):
 
         self.__init__(self.OriginalInputFileName)
         self.json_sim = {
-            "SIM_NAME"   : sim_dict["simulation_name"],
+            "SIM_NAME": sim_dict["simulation_name"],
             "NODE_VALUES": node_values,
             "LINK_VALUES": link_values
         }
         return self.json_sim
 
     def get_time_step(self, pattern_id=1):
-        '''
+        """
         returns the time_step of the network in minutes
         :param pattern_id:
         :return:
-        '''
-        return (24*60)/ENSim._getNcheck(self.ENgetpatternlen(pattern_id))
-
-
-
+        """
+        return (24 * 60) / ENSim._getncheck(self.ENgetpatternlen(pattern_id))
 
     def plot(self, json_data):
-        '''
+        """
         utility function used to plot data from network simulations
         WIP
         :param json_data:
         :return:
-        '''
+        """
 
         values = json_data["NODE_VALUES"]
         date_range = pd.date_range('1/1/2018', periods=97, freq='15min')
@@ -231,7 +226,6 @@ class ENSim(EPANetSimulation):
         fig = tools.make_subplots(1, 2, subplot_titles=('Emitter Value = 0', 'Emitter Value = 760'))
 
         time_step = self.get_time_step()
-
 
         trace = []
         for vals in data1:
@@ -262,32 +256,31 @@ class ENSim(EPANetSimulation):
         except IOError:
             print("Could not write to file {}".format(path))
 
-
     @staticmethod
     def write_json(output_json):
 
-        str = json.dumps(output_json)
+        json_data = json.dumps(output_json)
         with open("data.json", "wt") as f:
-            f.write(str)
+            f.write(json_data)
 
     @staticmethod
-    def _getNcheck(ret_val):
+    def _getncheck(ret_val):
 
         # check the return code
-        if  isinstance(ret_val, list):
+        if isinstance(ret_val, list):
             if ret_val[0] == 0:
                 # everything OK
                 return ret_val[1]
             else:
-                err_msg = ENgeterror(ret_val[0],100)
+                err_msg = ENgeterror(ret_val[0], 100)
                 raise EpanetError(err_msg)
         else:
             if ret_val is not 0:
                 err_msg = ENgeterror(ret_val, 100)
                 raise EpanetError(err_msg)
 
-def ENcheck(func):
 
+def en_check(func):
     def func_wrapper(*args):
         ret_val = func(args)
 
@@ -305,7 +298,6 @@ def ENcheck(func):
                 raise EpanetError(err_msg)
 
 
-
 class EpanetError(Exception):
 
     def __init__(self, err_msg):
@@ -313,7 +305,6 @@ class EpanetError(Exception):
 
 
 if __name__ == '__main__':
-
     es = ENSim("data/hanoi.inp")
 
     emitters = [(5, 0),
@@ -325,7 +316,7 @@ if __name__ == '__main__':
     query_dict = {
         "simulation_name": "Hanoi simulation",
         "simulation_type": "H",
-        "emitter_values" : emitters,
+        "emitter_values": emitters,
         "query": {
 
             "nodes": ["EN_PRESSURE", "EN_DEMAND"],
@@ -334,8 +325,6 @@ if __name__ == '__main__':
 
     }
 
-
     data = es.query_network(query_dict)
     es.plot(data)
     es.save_data("emitter_simulations.json")
-
