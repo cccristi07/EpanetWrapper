fa3255587849c93495f9420f01eafd927a7fdd25
Cazan Cristian <cristian.cazan@sparktech.ro>
2018-04-25 13:40:26 +0300

    dict learning

--- ENWrapper/plot_data.py
+++ ENWrapper/plot_data.py
@@ -4,25 +4,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.neural_network import MLPClassifier as nn
 from sklearn.preprocessing import normalize
+from sklearn.svm import SVC
 
-# descending order of nodes
-# nodes = [6, 13, 29, 22,  5, 25, 26, 14, 12, 30,  4, 21, 24, 23,  7, 18, 27, 28,  8, 19, 20, 15,  3, 16,  9,  1,  2, 17, 10, 11,  0]
+# dict learning
+from sklearn.decomposition import MiniBatchDictionaryLearning, DictionaryLearning
+# ascending order of nodes - Neural Net
+nodes = [6, 13, 29, 22,  5, 25, 26, 14, 12, 30,  4, 21, 24, 23,  7, 18, 27, 28,  8, 19, 20, 15,  3, 16,  9,  1,  2, 17, 10, 11,  0]
 
 # most important nodes
 # nodes = [1,  2, 17, 10, 11,  0]
 
-# least important nodes
-nodes = [2, 17, 10, 11, 0]
+# # least important nodes
+# nodes = [2, 17, 10, 11, 0]
+
+
+# ascending order of nodes -SVM Linear
+
+nodes = [30, 25, 15, 26, 11, 27, 22,  3, 19, 13, 10,  1,  7,  4, 12, 23,  0,  6, 20,  9, 14, 21, 18,  5, 8,  2, 24, 17, 16, 28, 29]
+
+
+ELEMENT = "NODE_VALUES"
+FEATURE = "EN_PRESSURE"
 
 
 with open("/home/spark/train_set.json") as f:
     json_str = f.read()
 
-
-
 data = json.loads(json_str)
 
-ref = np.array(data["NODE_VALUES"][0]["EN_PRESSURE"])
+ref = np.array(data[ELEMENT][0][FEATURE])
 
 with open("/home/spark/test2_set.json") as f:
     json_str = f.read()
@@ -38,14 +48,14 @@ X_test = []
 y_test = []
 
 
-for val in data["NODE_VALUES"][1:]:
+for val in data[ELEMENT][1:]:
 
     # plt.figure()
     # plt.plot(np.array(val["EN_PRESSURE"]) - ref)
     # plt.title("Demand in node {} with emitter {}".format(val["EMITTER_NODE"], val["EMITTER_VAL"]))
 
 
-    residue = np.array(val["EN_PRESSURE"]) - ref
+    residue = np.array(val[FEATURE]) - ref
     residue = residue[:, nodes]
     y.append(val["EMITTER_NODE"])
 
@@ -58,12 +68,12 @@ for val in data["NODE_VALUES"][1:]:
 
     X.append(residue)
 
-for val in testdata["NODE_VALUES"][1:]:
+for val in testdata[ELEMENT][1:]:
     # plt.figure()
     # plt.plot(np.array(val["EN_PRESSURE"]) - ref)
     # plt.title("Demand in node {} with emitter {}".format(val["EMITTER_NODE"], val["EMITTER_VAL"]))
 
-    residue = np.array(val["EN_PRESSURE"]) - ref
+    residue = np.array(val[FEATURE]) - ref
     residue = residue[:, nodes]
     y_test.append(val["EMITTER_NODE"])
 
@@ -93,17 +103,35 @@ X = np.squeeze(X, 0)
 
 
 network = nn(hidden_layer_sizes=(100,), activation='relu', warm_start=True, verbose=True)
+svm = SVC(kernel='linear',C=2.5,verbose=True, max_iter=200)
+svm.fit(X, y)
+feature_names = ["node" + str(no) for no in range(0, 31)]
+
+def f_importances(coef, names):
+    imp = coef
+    imp,names = zip(*sorted(zip(imp,names)))
+    plt.barh(range(len(names)), imp, align='center')
+    plt.yticks(range(len(names)), names)
+    plt.show()
+
+
 
 for epoch in range(1500):
+    #network.fit(X, y)
+    pass
 
-    network.fit(X, y)
 
-node_importance = np.sum(np.abs(network.coefs_[0]), axis=1)
-print(len(node_importance))
+weights = svm.coef_
+
+node_importance = np.sum(np.absolute(weights), axis=0)
 node_importance = node_importance / np.linalg.norm(node_importance)
-print(node_importance / np.linalg.norm(node_importance))
+f_importances(node_importance, feature_names)
 
-y_pred = network.predict(X_test)
+
+imp_nodes = np.argsort(node_importance)
+print(imp_nodes)
+
+y_pred = svm.predict(X_test)
 
 y_pred = y_pred - y_test
 corect = len([ x for x in y_pred if x == 0])
@@ -112,3 +140,5 @@ acc = corect / len(y_pred)
 print("Test accuracy is {}%".format(acc*100))
 # plt.plot(np.log(node_importance), 'rx')
 # plt.show()
+
+# TODO add dictionary learning method
